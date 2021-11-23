7fd21df1699ac27ae7fc278c54f4aeb4b2832982
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-11-23 01:27:08 -0500

    committing utility scripts

---
+++ start.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ -f log.txt ]; then
+	echo "Previous log file detected"
+	mkdir log_archive
+	t=$(date -r log.txt "+%m.%d.%Y::%H:%M:%S")
+	gzip log.txt
+	mv -v log.txt.gz log_archive/log.$t.txt.gz
+fi
+
+screen -dmLS _Wheatley npm start
+
+echo "Wheatley started in screen"
