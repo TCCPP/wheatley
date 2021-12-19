4c6e13994942fd51e052e11ce14962ecf6b92dfe
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2021-12-19 11:41:02 -0500

    Updated startup script

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 if [ -f log.txt ]; then
 	echo "Previous log file detected"
-	mkdir log_archive
+	[ ! -d log_archive ] && mkdir log_archive
 	t=$(date -r log.txt "+%m.%d.%Y::%H:%M:%S")
 	gzip log.txt
 	mv -v log.txt.gz log_archive/log.$t.txt.gz
