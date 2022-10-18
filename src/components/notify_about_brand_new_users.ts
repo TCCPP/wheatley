20b9683f0561236b63f9fdde80271af65c14602e
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-10-17 21:01:19 -0400

    Updated to log in the welcome channel

--- src/components/notify_about_brand_new_users.ts
+++ src/components/notify_about_brand_new_users.ts
@@ -1,13 +1,13 @@
 import * as Discord from "discord.js";
 import { strict as assert } from "assert";
-import { critical_error, fetch_text_channel, M } from "../utility/utils";
-import { colors, member_log_channel_id, MINUTE } from "../common";
+import { critical_error, diff_to_human, fetch_text_channel, M } from "../utility/utils";
+import { colors, MINUTE, welcome_channel_id } from "../common";
 
-const NEW_USER_THRESHOLD = MINUTE * 60;
+const NEW_USER_THRESHOLD = MINUTE * 30;
 
 let member_log_channel: Discord.TextChannel;
 
-function notify_about_brand_new_user(member: Discord.GuildMember) {
+async function notify_about_brand_new_user(member: Discord.GuildMember) {
     const embed = new Discord.EmbedBuilder()
         .setColor(colors.alert_color)
         .setAuthor({
@@ -15,21 +15,22 @@ function notify_about_brand_new_user(member: Discord.GuildMember) {
             iconURL: member.user.displayAvatarURL()
         })
         .setDescription(`User <@${member.user.id}>'s account was created at created at:`
-                      + ` <t:${Math.round(member.user.createdTimestamp / 1000)}>`)
+                      + ` <t:${Math.round(member.user.createdTimestamp / 1000)}>\n`
+                      + `Account age: ${diff_to_human(Date.now() - member.user.createdTimestamp)}`)
         .setFooter({
             text: `ID: ${member.id}`
         })
         .setTimestamp();
-    member_log_channel!.send({ embeds: [embed] })
+    await member_log_channel!.send({ embeds: [embed] })
         .catch((...args: any[]) => critical_error(...args));
     //member_log_channel!.send(`<@!${zelis_id}>`);
 }
 
-function on_join(member: Discord.GuildMember) {
+async function on_join(member: Discord.GuildMember) {
     try {
         assert(Date.now() - member.user.createdTimestamp >= 0);
         if(Date.now() - member.user.createdTimestamp <= NEW_USER_THRESHOLD) {
-            notify_about_brand_new_user(member);
+            await notify_about_brand_new_user(member);
         }
     } catch(e) {
         critical_error(e);
@@ -40,7 +41,7 @@ export async function setup_notify_about_brand_new_users(client: Discord.Client)
     M.debug("Setting up notify_about_brand_new_users");
     client.on("ready", async () => {
         try {
-            member_log_channel = await fetch_text_channel(member_log_channel_id);
+            member_log_channel = await fetch_text_channel(welcome_channel_id);
             M.debug("notify_about_brand_new_users: member_log_channel channel fetched");
             client.on("guildMemberAdd", on_join);
         } catch(e) {
