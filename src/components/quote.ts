1e318fe6bd9f9b7531cec9ed126522f71a0cac79
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-11-17 22:58:31 -0500

    Added implicit quote system

--- src/components/quote.ts
+++ src/components/quote.ts
@@ -1,6 +1,6 @@
 import * as Discord from "discord.js";
 import { strict as assert } from "assert";
-import { index_of_first_not_satisfying, is_image_link_embed, M } from "../utils";
+import { critical_error, index_of_first_not_satisfying, is_image_link_embed, M } from "../utils";
 import { MINUTE, TCCPP_ID } from "../common";
 import { decode_snowflake, forge_snowflake } from "./snowflake";
 import { BotComponent } from "../bot_component";
@@ -9,9 +9,17 @@ import { Command, CommandBuilder } from "../command";
 
 // https://discord.com/channels/331718482485837825/802541516655951892/877257002584252426
 //                              guild              channel            message
-const url_re = /^https:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)$/i;
+const raw_url_re = /https:\/\/discord\.com\/channels\/(\d+)\/(\d+)\/(\d+)/;
+const url_re = new RegExp(`^${raw_url_re.source}$`, "i");
+const implicit_quote_re = new RegExp(`\\[${raw_url_re.source}(b?)\\]`, "gi");
 
 const color = 0x7E78FE; //0xA931FF;
+
+type QuoteDescriptor = {
+    channel_id: string;
+    message_id: string;
+    block: boolean;
+};
 
 export class Quote extends BotComponent {
     constructor(wheatley: Wheatley) {
@@ -32,11 +40,15 @@ export class Quote extends BotComponent {
     async quote(command: Command, url: string) {
         const match = url.trim().match(url_re);
         if(match != null) {
-            M.log("Received quote command", url, command.get_or_forge_url());
+            M.log("Received quote command", command.user.tag, command.user.id, url, command.get_or_forge_url());
             assert(match.length == 4);
             const [ guild_id, channel_id, message_id ] = match.slice(1);
             if(guild_id == TCCPP_ID) {
-                await this.do_quote(command, channel_id, message_id, command.name == "quoteb");
+                await this.do_quote(command, [{
+                    channel_id,
+                    message_id,
+                    block: command.name == "quoteb"
+                }]);
             }
         } else {
             command.reply("Usage: `!quote <url>`\n"
@@ -47,8 +59,33 @@ export class Quote extends BotComponent {
     override async on_message_create(message: Discord.Message) {
         if(message.author.id == this.wheatley.client.user!.id) return; // Ignore self
         if(message.author.bot) return; // Ignore bots
-        //if(message.guildId != TCCPP_ID) return; // Ignore messages outside TCCPP (e.g. dm's)
-
+        if(message.guildId != TCCPP_ID) return; // Ignore messages outside TCCPP (e.g. dm's)
+        if(message.content.includes("[https://")) {
+            // if the message might contain a link, look at it
+            const quote_descriptors = [...message.content.matchAll(implicit_quote_re)]
+                .filter(([ _, guild_id ]) => guild_id == TCCPP_ID)
+                .map(arr => arr.slice(2))
+                .map(([ channel_id, message_id, block_flag ]) => ({
+                    channel_id,
+                    message_id,
+                    block: block_flag == "b"
+                }));
+            if(quote_descriptors.length >= 1) {
+                M.log(
+                    "Implicit quote request",
+                    message.author.tag,
+                    message.author.id,
+                    ...quote_descriptors.map(d => `${d.channel_id}/${d.message_id}` + (d.block ? " block" : "")),
+                    message.url
+                );
+                const command = new Command("quote", message, this.wheatley);
+                await this.do_quote(command, quote_descriptors);
+                const reply = command.get_reply();
+                assert(reply instanceof Discord.Message);
+                this.wheatley.make_deletable(message, reply);
+                await message.suppressEmbeds();
+            }
+        }
     }
 
     // TODO: Redundant with server_suggestion_tracker
@@ -108,42 +145,49 @@ export class Quote extends BotComponent {
         return [ embed, ...image_embeds, ...other_embeds ];
     }
 
-    async do_quote(command: Command, channel_id: string, message_id: string, block: boolean) {
-        const channel = await this.wheatley.TCCPP.channels.fetch(channel_id);
-        if(channel instanceof Discord.TextChannel
-        || channel instanceof Discord.ThreadChannel
-        || channel instanceof Discord.NewsChannel) {
-            let messages: Discord.Message[] = [];
-            if(block) {
-                const fetched_messages = (await channel.messages.fetch({
-                    after: forge_snowflake(decode_snowflake(message_id) - 1),
-                    limit: 50
-                })).map(m => m).reverse();
-                const start_time = fetched_messages.length > 0 ? fetched_messages[0].createdTimestamp : undefined;
-                const end = index_of_first_not_satisfying(fetched_messages,
-                                                          m => m.author.id == fetched_messages[0].author.id
-                                                               && m.createdTimestamp - start_time! <= 60 * MINUTE);
-                messages = fetched_messages.slice(0, end == -1 ? fetched_messages.length : end);
+    async do_quote(command: Command, messages: QuoteDescriptor[]) {
+        const embeds: (Discord.EmbedBuilder | Discord.Embed)[] = [];
+        for(const { channel_id, message_id, block } of messages) {
+            const channel = await this.wheatley.TCCPP.channels.fetch(channel_id);
+            if(channel instanceof Discord.TextChannel
+            || channel instanceof Discord.ThreadChannel
+            || channel instanceof Discord.NewsChannel) {
+                let messages: Discord.Message[] = [];
+                if(block) {
+                    const fetched_messages = (await channel.messages.fetch({
+                        after: forge_snowflake(decode_snowflake(message_id) - 1),
+                        limit: 50
+                    })).map(m => m).reverse();
+                    const start_time = fetched_messages.length > 0 ? fetched_messages[0].createdTimestamp : undefined;
+                    const end = index_of_first_not_satisfying(fetched_messages,
+                                                              m => m.author.id == fetched_messages[0].author.id
+                                                                   && m.createdTimestamp - start_time! <= 60 * MINUTE);
+                    messages = fetched_messages.slice(0, end == -1 ? fetched_messages.length : end);
+                } else {
+                    const quote_message = await channel.messages.fetch(message_id);
+                    messages = [quote_message];
+                }
+                assert(messages.length >= 1);
+                const quote_embeds = await this.make_quote(messages, await command.get_member());
+                embeds.push(...quote_embeds);
             } else {
-                const quote_message = await channel.messages.fetch(message_id);
-                messages = [quote_message];
+                embeds.push(new Discord.EmbedBuilder().setDescription("Error: Channel not a text channel"));
+                critical_error("Error: Channel not a text channel");
             }
-            assert(messages.length >= 1);
-            const quote_embeds = await this.make_quote(messages, await command.get_member());
-            await command.reply({ embeds: quote_embeds });
+        }
+        if(embeds.length > 0) {
+            await command.reply({ embeds: embeds });
             // log
             // TODO: Can probably improve how this is done. Figure out later.
-            this.wheatley.staff_message_log.send({
+            /*this.wheatley.staff_message_log.send({
                 content: "Message quoted"
                         + `\nIn <#${command.channel_id}> ${command.get_or_forge_url()}`
                         + `\nFrom <#${channel_id}> ${messages[0].url}`
                         + `\nBy ${command.user.tag} ${command.user.id}`,
-                embeds: quote_embeds
-            });
-            // delete request
-            ///message.delete();
+                embeds
+            });*/
         } else {
-            await command.reply("Error: Channel not a text channel.", true);
+            throw "No quote embeds";
         }
     }
 }
