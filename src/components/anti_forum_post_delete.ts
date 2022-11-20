7ca7a0659c60c70616c3d7f110b69b745b0e38f9
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2022-11-20 15:43:45 -0500

    Updated anti forum post behavior to mark threads as solved

--- src/components/anti_forum_post_delete.ts
+++ src/components/anti_forum_post_delete.ts
@@ -2,9 +2,9 @@ import { strict as assert } from "assert";
 
 import * as Discord from "discord.js";
 
-import { M } from "../utils";
+import { get_tag, M } from "../utils";
 import { BotComponent } from "../bot_component";
-import { colors } from "../common";
+import { colors, is_forum_help_thread } from "../common";
 
 function create_embed(title: string | undefined, color: number, msg: string) {
     const embed = new Discord.EmbedBuilder()
@@ -19,18 +19,29 @@ function create_embed(title: string | undefined, color: number, msg: string) {
 export class AntiForumPostDelete extends BotComponent {
     override async on_message_delete(message: Discord.Message<boolean> | Discord.PartialMessage) {
         if(message.channel.id == message.id) {
-            M.log(`Firing AntiForumPostDelete on ${message.channel.url}`);
-            await message.channel.send({
-                content: `<@${message.author?.id}>`,
-                embeds: [
-                    create_embed(
-                        "Please Do Not Delete Posts!",
-                        colors.red,
-                        "Please don't delete forum posts. They can be helpful to refer to later and other members can"
-                        + " learn from them. You can use `!solved` to close a post and mark it as solved."
-                    )
-                ]
-            });
+            assert(message.channel.isThread());
+            const thread = message.channel;
+            if(is_forum_help_thread(thread)) {
+                const forum = thread.parent;
+                assert(forum instanceof Discord.ForumChannel);
+                const open_tag = get_tag(forum, "Open").id;
+                const solved_tag = get_tag(forum, "Solved").id;
+                M.log(`Firing AntiForumPostDelete on ${thread.url}`);
+                await thread.send({
+                    content: `<@${message.author?.id}>`,
+                    embeds: [
+                        create_embed(
+                            "Please Do Not Delete Posts!",
+                            colors.red,
+                            "Please don't delete forum posts. They can be helpful to refer to later and other members"
+                            + " can learn from them. You can use `!solved` to close a post and mark it as solved."
+                        )
+                    ]
+                });
+                if(!thread.appliedTags.includes(solved_tag)) {
+                    await thread.setAppliedTags([solved_tag].concat(thread.appliedTags.filter(t => t != open_tag)));
+                }
+            }
         }
     }
 }
