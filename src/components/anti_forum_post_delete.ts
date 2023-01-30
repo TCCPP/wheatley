68cd4f45f226dce6b1a37597540dd3ca4f95a802
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2023-01-30 11:12:35 -0500

    Update anti forum post delete logic

--- src/components/anti_forum_post_delete.ts
+++ src/components/anti_forum_post_delete.ts
@@ -4,7 +4,7 @@ import * as Discord from "discord.js";
 
 import { get_tag, M } from "../utils";
 import { BotComponent } from "../bot_component";
-import { colors, is_forum_help_thread } from "../common";
+import { colors, is_forum_help_thread, MINUTE } from "../common";
 
 function create_embed(title: string | undefined, color: number, msg: string) {
     const embed = new Discord.EmbedBuilder()
@@ -21,11 +21,14 @@ export class AntiForumPostDelete extends BotComponent {
         if(message.channel.id == message.id) {
             assert(message.channel.isThread());
             const thread = message.channel;
-            if(is_forum_help_thread(thread)) {
+            console.log(thread.messageCount);
+            if(is_forum_help_thread(thread)
+            && (Date.now() - thread.createdTimestamp! > 2 * MINUTE || thread.messageCount! > 0)) {
                 const forum = thread.parent;
                 assert(forum instanceof Discord.ForumChannel);
                 const open_tag = get_tag(forum, "Open").id;
                 const solved_tag = get_tag(forum, "Solved").id;
+                const stale_tag = get_tag(forum, "Stale").id;
                 M.log(`Firing AntiForumPostDelete on ${thread.url}`);
                 await thread.send({
                     content: `<@${message.author?.id}>`,
@@ -34,13 +37,19 @@ export class AntiForumPostDelete extends BotComponent {
                             "Please Do Not Delete Posts!",
                             colors.red,
                             "Please don't delete forum posts. They can be helpful to refer to later and other members"
-                            + " can learn from them. You can use `!solved` to close a post and mark it as solved."
+                            + " can learn from them. In the future you can use `!solved` to close a post and mark a"
+                            + " post as solved."
                         )
                     ]
                 });
-                if(!thread.appliedTags.includes(solved_tag)) {
-                    await thread.setAppliedTags([solved_tag].concat(thread.appliedTags.filter(t => t != open_tag)));
+                if(!thread.appliedTags.includes(stale_tag)) {
+                    await thread.setAppliedTags(
+                        [stale_tag].concat(thread.appliedTags.filter(t => ![open_tag, solved_tag].includes(t)))
+                    );
                 }
+                await thread.setArchived(true);
+            } else {
+                await thread.delete();
             }
         }
     }
