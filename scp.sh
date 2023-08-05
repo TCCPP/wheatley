b285440f16074911640deb4657bf644945f0cdc2
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-08-05 18:48:16 -0400

    Update eslint rules and a deploy script

--- scp.sh
+++ scp.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 #scp -r package.json package-lock.json tsconfig.json src d0:Projects/Wheatley
-rsync -av --checksum db_backup.sh package.json package-lock.json start.sh run-persist.sh tsconfig.json indexes src test wiki_articles auth.json d0:Projects/wheatley --exclude={"indexes/cppref/*.txt","indexes/cppref/*cppreference*","*.js","src/wheatley-private/.git"}
+rsync -av --checksum db_backup.sh package.json package-lock.json start.sh run-persist.sh tsconfig.json indexes src test wiki_articles auth.json scripts d0:Projects/wheatley --exclude={"indexes/cppref/*.txt","indexes/cppref/*cppreference*","*.js","src/wheatley-private/.git"}
 #rsync -av -I package.json package-lock.json tsconfig.json cppref src test d0:Projects/Wheatley --exclude={"cppref/*.txt","cppref/*cppreference*"}
