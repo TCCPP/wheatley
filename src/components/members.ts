5530374efc3be998f4d9398f66242ee191a1138e
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-09-13 11:51:44 -0400

    Add !members

---
+++ src/components/members.ts
@@ -0,0 +1,39 @@
+import * as Discord from "discord.js";
+
+import { strict as assert } from "assert";
+
+import { M } from "../utils/debugging-and-logging.js";
+import { colors } from "../common.js";
+import { BotComponent } from "../bot-component.js";
+import { Wheatley } from "../wheatley.js";
+import { TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
+import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
+
+/**
+ * Adds a !members.
+ */
+export default class Members extends BotComponent {
+    static override get is_freestanding() {
+        return true;
+    }
+
+    constructor(wheatley: Wheatley) {
+        super(wheatley);
+
+        this.add_command(
+            new TextBasedCommandBuilder("members").set_description("Members").set_handler(this.members.bind(this)),
+        );
+    }
+
+    async members(command: TextBasedCommand) {
+        M.log("Received members command");
+        await command.reply({
+            embeds: [
+                new Discord.EmbedBuilder()
+                    .setColor(colors.wheatley)
+                    .setTitle("Members")
+                    .setDescription(`Total: ${this.wheatley.TCCPP.members.cache.size} members`),
+            ],
+        });
+    }
+}
