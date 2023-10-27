bf8c534ae588548ced9d02be8ceec7ce1e45d4b2
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2023-10-27 10:35:04 -0400

    Format fixes

--- src/components/c-help-redirect.ts
+++ src/components/c-help-redirect.ts
@@ -10,269 +10,249 @@ import { M } from "../utils/debugging-and-logging.js";
 const code_block_start = "```";
 
 const cpp_keywords = [
-  "std::", //just :: would work too, this is slightly less prone to false positives
-  "using namespace ",
-  "class ",
-  "typename ",
-  "template",
-  "virtual ",
-  "dynamic_cast",
-  "static_cast",
-  "const_cast",
-  "reinterpret_cast",
-  "= new ", //too common to include just "new"
-  //"delete ", //too common
-  "delete[]",
-  "public:",
-  "protected:",
-  "private:",
+    "std::", //just :: would work too, this is slightly less prone to false positives
+    "using namespace ",
+    "class ",
+    "typename ",
+    "template",
+    "virtual ",
+    "dynamic_cast",
+    "static_cast",
+    "const_cast",
+    "reinterpret_cast",
+    "= new ", //too common to include just "new"
+    //"delete ", //too common
+    "delete[]",
+    "public:",
+    "protected:",
+    "private:",
 ];
 
 const not_c_keywords = [
-  "explicit",
-  "mutable",
-  "final",
-  "try",
-  "catch",
-  "throw",
-  "operator", //possibly list all operator+ etc. to be on the safe side
-  "cout<<",
-  "cout <<",
-  "cin>>",
-  "cin >>",
-  "<iostream>",
-  "<cstdio>",
+    "explicit",
+    "mutable",
+    "final",
+    "try",
+    "catch",
+    "throw",
+    "operator", //possibly list all operator+ etc. to be on the safe side
+    "cout<<",
+    "cout <<",
+    "cin>>",
+    "cin >>",
+    "<iostream>",
+    "<cstdio>",
 ];
 
 const maybe_c_keywords = [
-  "printf",
-  "scanf",
-  "<stdio.h>",
-  "<stdlib.h>",
-  "<math.h>",
-  "malloc",
-  "calloc",
-  "realloc",
-  "free(", //too common to include just "free"
+    "printf",
+    "scanf",
+    "<stdio.h>",
+    "<stdlib.h>",
+    "<math.h>",
+    "malloc",
+    "calloc",
+    "realloc",
+    "free(", //too common to include just "free"
 ];
 
 /**
  * Checks for cpp code in #c-help-text and suggests #cpp-help-text instead, and vice versa
  */
 export default class CHelpRedirect extends BotComponent {
-  constructor(wheatley: Wheatley) {
-    super(wheatley);
+    constructor(wheatley: Wheatley) {
+        super(wheatley);
 
-    this.add_command(
-      new TextBasedCommandBuilder("not-c")
-        .set_description("Mark C++ code in the C help channel")
-        .add_user_option({
-          title: "user",
-          description: "User who posted the code",
-          required: false,
-        })
-        .set_handler(this.not_c.bind(this)),
-    );
+        this.add_command(
+            new TextBasedCommandBuilder("not-c")
+                .set_description("Mark C++ code in the C help channel")
+                .add_user_option({
+                    title: "user",
+                    description: "User who posted the code",
+                    required: false,
+                })
+                .set_handler(this.not_c.bind(this)),
+        );
 
-    this.add_command(
-      new TextBasedCommandBuilder("not-cpp")
-        .set_description("Mark C code in the C++ help channel")
-        .add_user_option({
-          title: "user",
-          description: "User who posted the code",
-          required: false,
-        })
-        .set_handler(this.not_cpp.bind(this)),
-    );
-  }
+        this.add_command(
+            new TextBasedCommandBuilder("not-cpp")
+                .set_description("Mark C code in the C++ help channel")
+                .add_user_option({
+                    title: "user",
+                    description: "User who posted the code",
+                    required: false,
+                })
+                .set_handler(this.not_cpp.bind(this)),
+        );
+    }
 
-  async is_not_new_member(message: Discord.Message) {
-    let member: Discord.GuildMember;
-    if (message.member == null) {
-      try {
-        member = await message.guild!.members.fetch(message.author.id);
-      } catch (error) {
-        M.warn("Failed to get user", message.author.id);
+    async is_not_new_member(message: Discord.Message) {
+        let member: Discord.GuildMember;
+        if (message.member == null) {
+            try {
+                member = await message.guild!.members.fetch(message.author.id);
+            } catch (error) {
+                M.warn("Failed to get user", message.author.id);
+                return false;
+            }
+        } else {
+            member = message.member;
+        }
+        assert(member.joinedTimestamp != null);
+        return Date.now() - member.joinedTimestamp >= 7 * DAY;
+    }
+
+    check_message_for_cpp_code(message: Discord.Message): boolean {
+        if (!message.content.includes(code_block_start)) {
+            // to avoid false positives, only check inside code blocks
+            return false;
+        }
+
+        let text = message.content;
+
+        while (text.search(code_block_start) > -1) {
+            const start = text.search(code_block_start);
+            const end = text.substring(start + code_block_start.length).search(code_block_start);
+            const block = text.substring(start + code_block_start.length, start + code_block_start.length + end);
+
+            for (const keyword of cpp_keywords) {
+                if (block.includes(keyword)) {
+                    return true;
+                }
+            }
+
+            text = text.substring(start + code_block_start.length + end + code_block_start.length);
+        }
         return false;
-      }
-    } else {
-      member = message.member;
-    }
-    assert(member.joinedTimestamp != null);
-    return Date.now() - member.joinedTimestamp >= 7 * DAY;
-  }
-
-  check_message_for_cpp_code(message: Discord.Message): boolean {
-    if (!message.content.includes(code_block_start)) {
-      // to avoid false positives, only check inside code blocks
-      return false;
     }
 
-    let text = message.content;
-
-    while (text.search(code_block_start) > -1) {
-      const start = text.search(code_block_start);
-      const end = text
-        .substring(start + code_block_start.length)
-        .search(code_block_start);
-      const block = text.substring(
-        start + code_block_start.length,
-        start + code_block_start.length + end,
-      );
-
-      for (const keyword of cpp_keywords) {
-        if (block.includes(keyword)) {
-          return true;
+    check_message_for_c_code(message: Discord.Message): boolean {
+        if (!message.content.includes(code_block_start)) {
+            // to avoid false positives, only check inside code blocks
+            return false;
         }
-      }
 
-      text = text.substring(
-        start + code_block_start.length + end + code_block_start.length,
-      );
-    }
-    return false;
-  }
+        let text = message.content;
 
-  check_message_for_c_code(message: Discord.Message): boolean {
-    if (!message.content.includes(code_block_start)) {
-      // to avoid false positives, only check inside code blocks
-      return false;
-    }
+        while (text.search(code_block_start) > -1) {
+            const start = text.search(code_block_start);
+            const end = text.substring(start + code_block_start.length).search(code_block_start);
+            const block = text.substring(start + code_block_start.length, start + code_block_start.length + end);
 
-    let text = message.content;
+            //for C code, the block must not have any C++ keywords
+            //including inconclusive keywords
+            //and have some C keyword
 
-    while (text.search(code_block_start) > -1) {
-      const start = text.search(code_block_start);
-      const end = text
-        .substring(start + code_block_start.length)
-        .search(code_block_start);
-      const block = text.substring(
-        start + code_block_start.length,
-        start + code_block_start.length + end,
-      );
+            let c_code_found = false;
+            let cpp_code_found = false;
 
-      //for C code, the block must not have any C++ keywords
-      //including inconclusive keywords
-      //and have some C keyword
+            for (const keyword of cpp_keywords) {
+                if (block.includes(keyword)) {
+                    cpp_code_found = true;
+                }
+            }
 
-      let c_code_found = false;
-      let cpp_code_found = false;
+            for (const keyword of not_c_keywords) {
+                if (block.includes(keyword)) {
+                    cpp_code_found = true;
+                }
+            }
 
-      for (const keyword of cpp_keywords) {
-        if (block.includes(keyword)) {
-          cpp_code_found = true;
+            for (const keyword of maybe_c_keywords) {
+                if (block.includes(keyword)) {
+                    c_code_found = true;
+                }
+            }
+
+            if (c_code_found && !cpp_code_found) {
+                return true;
+            }
+
+            text = text.substring(start + code_block_start.length + end + code_block_start.length);
         }
-      }
+        return false;
+    }
 
-      for (const keyword of not_c_keywords) {
-        if (block.includes(keyword)) {
-          cpp_code_found = true;
+    async not_c(command: TextBasedCommand, user: Discord.User | null) {
+        assert(command.channel);
+        assert(command.channel instanceof Discord.GuildChannel);
+
+        // Only allowed in #c-help-text
+        if (command.channel.id != this.wheatley.channels.c_help_text.id) {
+            await command.reply(`Can only be used in <#${this.wheatley.channels.c_help_text.id}>`, true);
+            return;
         }
-      }
 
-      for (const keyword of maybe_c_keywords) {
-        if (block.includes(keyword)) {
-          c_code_found = true;
+        //for manual triggers, trust the caller and don't check the message
+        //supposedly the automatic check didn't trigger, so checking the message again would fail again
+        if (user) {
+            await command.channel.send(
+                `<@${user.id}> Your code looks like C++ code, but this is a C channel. ` +
+                    `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
+            );
+        } else {
+            await command.channel.send(
+                `This code looks like C++ code, but this is a C channel. ` +
+                    `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
+            );
         }
-      }
-
-      if (c_code_found && !cpp_code_found) {
-        return true;
-      }
-
-      text = text.substring(
-        start + code_block_start.length + end + code_block_start.length,
-      );
-    }
-    return false;
-  }
-
-  async not_c(command: TextBasedCommand, user: Discord.User | null) {
-    assert(command.channel);
-    assert(command.channel instanceof Discord.GuildChannel);
-
-    // Only allowed in #c-help-text
-    if (command.channel.id != this.wheatley.channels.c_help_text.id) {
-      await command.reply(
-        `Can only be used in <#${this.wheatley.channels.c_help_text.id}>`,
-        true,
-      );
-      return;
     }
 
-    //for manual triggers, trust the caller and don't check the message
-    //supposedly the automatic check didn't trigger, so checking the message again would fail again
-    if (user) {
-      await command.channel.send(
-        `<@${user.id}> Your code looks like C++ code, but this is a C channel. ` +
-          `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
-      );
-    } else {
-      await command.channel.send(
-        `This code looks like C++ code, but this is a C channel. ` +
-          `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
-      );
-    }
-  }
+    async not_cpp(command: TextBasedCommand, user: Discord.User | null) {
+        assert(command.channel);
+        assert(command.channel instanceof Discord.GuildChannel);
 
-  async not_cpp(command: TextBasedCommand, user: Discord.User | null) {
-    assert(command.channel);
-    assert(command.channel instanceof Discord.GuildChannel);
+        // Only allowed in #cpp-help-text
+        if (command.channel.id != this.wheatley.channels.cpp_help_text.id) {
+            await command.reply(`Can only be used in <#${this.wheatley.channels.cpp_help_text.id}>`, true);
+            return;
+        }
 
-    // Only allowed in #cpp-help-text
-    if (command.channel.id != this.wheatley.channels.cpp_help_text.id) {
-      await command.reply(
-        `Can only be used in <#${this.wheatley.channels.cpp_help_text.id}>`,
-        true,
-      );
-      return;
+        //for manual triggers, trust the caller and don't check the message
+        //supposedly the automatic check didn't trigger, so checking the message again would fail again
+        if (user) {
+            await command.channel.send(
+                `<@${user.id}> Your code looks like C code, but this is a C++ channel. ` +
+                    `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
+            );
+        } else {
+            await command.channel.send(
+                `This code looks like C code, but this is a C++ channel. ` +
+                    `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
+            );
+        }
     }
 
-    //for manual triggers, trust the caller and don't check the message
-    //supposedly the automatic check didn't trigger, so checking the message again would fail again
-    if (user) {
-      await command.channel.send(
-        `<@${user.id}> Your code looks like C code, but this is a C++ channel. ` +
-          `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
-      );
-    } else {
-      await command.channel.send(
-        `This code looks like C code, but this is a C++ channel. ` +
-          `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
-      );
-    }
-  }
+    override async on_message_create(message: Discord.Message) {
+        // Ignore self, bots, and messages outside TCCPP (e.g. dm's)
+        if (
+            message.author.id == this.wheatley.client.user!.id ||
+            message.author.bot ||
+            message.guildId != this.wheatley.TCCPP.id
+        ) {
+            return;
+        }
 
-  override async on_message_create(message: Discord.Message) {
-    // Ignore self, bots, and messages outside TCCPP (e.g. dm's)
-    if (
-      message.author.id == this.wheatley.client.user!.id ||
-      message.author.bot ||
-      message.guildId != this.wheatley.TCCPP.id
-    ) {
-      return;
-    }
+        //only auto-check new members
+        if (await this.is_not_new_member(message)) {
+            return;
+        }
 
-    //only auto-check new members
-    if (await this.is_not_new_member(message)) {
-      return;
+        // Only check messages in help-text channels
+        if (message.channel.id == this.wheatley.channels.c_help_text.id) {
+            if (this.check_message_for_cpp_code(message)) {
+                await message.reply(
+                    `<@${message.author.id}> Your code looks like C++ code, but this is a C channel.` +
+                        `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
+                );
+            }
+        } else if (message.channel.id == this.wheatley.channels.cpp_help_text.id) {
+            if (this.check_message_for_c_code(message)) {
+                await message.reply(
+                    `<@${message.author.id}> Your code looks like C code, but this is a C++ channel.` +
+                        `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
+                );
+            }
+        }
     }
-
-    // Only check messages in help-text channels
-    if (message.channel.id == this.wheatley.channels.c_help_text.id) {
-      if (this.check_message_for_cpp_code(message)) {
-        await message.reply(
-          `<@${message.author.id}> Your code looks like C++ code, but this is a C channel.` +
-            `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
-        );
-      }
-    } else if (message.channel.id == this.wheatley.channels.cpp_help_text.id) {
-      if (this.check_message_for_c_code(message)) {
-        await message.reply(
-          `<@${message.author.id}> Your code looks like C code, but this is a C++ channel.` +
-            `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
-        );
-      }
-    }
-  }
 }
