861c827d2df961fb70dafb3a9f2b274f6661dab4
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2023-11-07 19:49:51 -0500

    Add !restart

---
+++ src/components/restart.ts
@@ -0,0 +1,36 @@
+import * as Discord from "discord.js";
+
+import { strict as assert } from "assert";
+
+import { M } from "../utils/debugging-and-logging.js";
+import { colors } from "../common.js";
+import { BotComponent } from "../bot-component.js";
+import { Wheatley } from "../wheatley.js";
+import { TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
+import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
+
+/**
+ * Adds a /restart command.
+ */
+export default class Restart extends BotComponent {
+    static override get is_freestanding() {
+        return true;
+    }
+
+    constructor(wheatley: Wheatley) {
+        super(wheatley);
+
+        this.add_command(
+            new TextBasedCommandBuilder("restart")
+                .set_permissions(Discord.PermissionFlagsBits.BanMembers)
+                .set_description("Restart")
+                .set_handler(this.restart.bind(this)),
+        );
+    }
+
+    async restart(command: TextBasedCommand) {
+        M.log("Received restart command");
+        await command.reply("Received restart command", true);
+        process.exit();
+    }
+}
