7a464e0a244958995a081bdc03068b95e0264f9f
Elias Schablowski <eschablowski@me.com>
2024-03-26 17:34:06 -0700

    Fixed typing issue

--- src/components/anti-everyone.ts
+++ src/components/anti-everyone.ts
@@ -7,6 +7,10 @@ import { SelfClearingMap } from "../utils/containers.js";
 
 const failed_everyone_re = /(?:@everyone|@here)/g; // todo: word boundaries?
 
+export interface AntiEveryoneMessageCache extends Discord.Message {
+    replyTo: Discord.Message["id"];
+}
+
 /**
  * Responds to users attempting to ping @everyone or @here
  * with a message discouraging the behavior.
@@ -14,10 +18,13 @@ const failed_everyone_re = /(?:@everyone|@here)/g; // todo: word boundaries?
 export default class AntiEveryone extends BotComponent {
     /**
      * Replies that have been made to users who attempted to ping everyone.
-     * 
+     *
      * @note This is limited to 50 replies, in order to keep memory usage down.
      */
-    public replies: SelfClearingMap<Discord.User, Discord.Message[]> = new SelfClearingMap<Discord.User, Discord.Message[]>(1000 * 60 * 60, 1000 * 60);
+    public replies: SelfClearingMap<Discord.User, AntiEveryoneMessageCache[]> = new SelfClearingMap(
+        1000 * 60 * 60,
+        1000 * 60,
+    );
     constructor(wheatley: Wheatley) {
         super(wheatley);
     }
@@ -38,13 +45,24 @@ export default class AntiEveryone extends BotComponent {
         if (message.content.match(failed_everyone_re) != null) {
             // NOTE: .toLocaleString("en-US") formats this number with commas.
             const memberCount = this.wheatley.TCCPP.members.cache.size.toLocaleString("en-US");
-            await message.reply({
+
+            // Store the reply for later deletion, if necessary
+            if (!this.replies.has(message.author)) {
+                this.replies.set(message.author, []);
+            }
+            const reply = await message.reply({
                 content: `Did you really just try to ping ${memberCount} people?`,
             });
 
-            // Store the reply for later deletion, if necessary
-            if(!this.replies.has(message.author)) this.replies.set(message.author, []);
-            this.replies.get(message.author)!.push(message);
+            // Store the reply for later deletion, along with the message it was replying to
+            this.replies.get(message.author)!.push(
+                Object.assign(
+                    {
+                        replyTo: message.id,
+                    },
+                    reply,
+                ),
+            );
         }
     }
 
@@ -54,7 +72,9 @@ export default class AntiEveryone extends BotComponent {
      * @TODO: Actually bind this into the anti-spam system
      */
     async deleteReplies(user: Discord.User) {
-        if(!this.replies.has(user)) return;
+        if (!this.replies.has(user)) {
+            return;
+        }
 
         const deletedAll = Promise.all(this.replies.get(user)!.map(reply => reply.delete()));
         this.replies.remove(user);
@@ -66,13 +86,16 @@ export default class AntiEveryone extends BotComponent {
      * @param message The message that was deleted
      */
     override async on_message_delete(message: Discord.Message<boolean>): Promise<void> {
-        if(Math.abs(Date.now() - message.createdTimestamp) > 1000) return; // Message was likely deleted by the user
+        if (Math.abs(Date.now() - message.createdTimestamp) > 1000) {
+            // Message was likely deleted by the user, rather than automatically
+            return;
+        }
 
         const author = message.author;
         const replies = this.replies.get(author);
-        const reply = replies?.find(reply => reply.reference?.messageId == message.id);
-        if(reply) {
-            reply.deleteReply();
+        const reply = replies?.find(reply => reply.replyTo == message.id);
+        if (reply) {
+            await reply.delete();
             this.replies.set(author, replies?.filter(reply => reply.id !== message.id) ?? []);
         }
     }
