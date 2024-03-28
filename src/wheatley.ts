387055fd1380659178bddfe28a203ddfc8d79312
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-03-27 19:50:25 -0500

    Channel update

--- src/wheatley.ts
+++ src/wheatley.ts
@@ -100,7 +100,7 @@ const channels_map = {
     staff_flag_log: tuple("1026972603019169842", Discord.TextChannel),
     staff_experimental_log: tuple("1207899185790197760", Discord.TextChannel),
     staff_action_log: tuple("845290775692443699", Discord.TextChannel),
-    staff_watch_log: tuple("1191147767733694585", Discord.TextChannel),
+    staff_clock_log: tuple("1220882759862452284", Discord.ForumChannel),
     welcome: tuple("778017793567490078", Discord.TextChannel),
     staff_member_log: tuple("875681819662622730", Discord.TextChannel),
     staff_message_log: tuple("467729928956411914", Discord.TextChannel),
