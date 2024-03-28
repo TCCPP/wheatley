19bc71ac071472acc9802b3bf369b756aa3917f4
Elias Schablowski <31389869+eschablowski@users.noreply.github.com>
2024-03-27 17:33:00 -0700

    Disable auto-reply if a link is detected in the output (#86)
    This disables the reply message if a link-like string is detected inside
    of `@everyone`/`@here` messages, mainly to not allow advertisements to
    stay up within Weatley's reply.

--- src/components/anti-everyone.ts
+++ src/components/anti-everyone.ts
@@ -1,16 +1,29 @@
 import * as Discord from "discord.js";
 import { BotComponent } from "../bot-component.js";
 import { Wheatley } from "../wheatley.js";
-import { colors } from "../common.js";
+import { colors, MINUTE, HOUR } from "../common.js";
 import { M } from "../utils/debugging-and-logging.js";
+import { SelfClearingMap } from "../utils/containers.js";
+import { unwrap } from "../utils/misc.js";
 
 const failed_everyone_re = /(?:@everyone|@here)/g; // todo: word boundaries?
+
+export interface AntiEveryoneMessageCache {
+    reply_to: Discord.Message["id"];
+    reply: Discord.Message;
+}
 
 /**
  * Responds to users attempting to ping @everyone or @here
  * with a message discouraging the behavior.
  */
 export default class AntiEveryone extends BotComponent {
+    /**
+     * Replies that have been made to users who attempted to ping everyone.
+     *
+     * @note This is limited to the last hour, in order to keep memory usage down.
+     */
+    replies = new SelfClearingMap<Discord.User, AntiEveryoneMessageCache[]>(HOUR, MINUTE);
     constructor(wheatley: Wheatley) {
         super(wheatley);
     }
@@ -31,9 +44,66 @@ export default class AntiEveryone extends BotComponent {
         if (message.content.match(failed_everyone_re) != null) {
             // NOTE: .toLocaleString("en-US") formats this number with commas.
             const memberCount = this.wheatley.TCCPP.members.cache.size.toLocaleString("en-US");
-            await message.reply({
+
+            // Store the reply for later deletion, if necessary
+            if (!this.replies.has(message.author)) {
+                this.replies.set(message.author, []);
+            }
+            const reply = await message.reply({
                 content: `Did you really just try to ping ${memberCount} people?`,
             });
+
+            // Store the reply for later deletion, along with the message it was replying to
+            unwrap(this.replies.get(message.author)).push({ reply_to: message.id, reply });
+        }
+    }
+
+    /**
+     * Deletes all `Did you really try to ping ... people?` replies that were made to a particular user
+     * @note this should be used to auto-hide spam message replies in order to try and reduce the effect of spam
+     * @TODO: Actually bind this into the anti-spam system
+     */
+    async delete_replies(user: Discord.User) {
+        if (!this.replies.has(user)) {
+            return;
+        }
+
+        // For loop to wait for each reply to be deleted, before moving on to the next one
+        for (const reply_cache of unwrap(this.replies.get(user))) {
+            try {
+                await reply_cache.reply.delete();
+            } catch (e) {
+                // If the message was already deleted, we don't need to do anything
+            }
+        }
+        // Remove the user from the cache, mainly to reduce clutter
+        this.replies.remove(user);
+    }
+
+    /**
+     * Auto-delete replies to messages that were deleted
+     * @param message The message that was deleted
+     */
+    override async on_message_delete(message: Discord.Message<boolean>): Promise<void> {
+        if (Math.abs(Date.now() - message.createdTimestamp) > 1000) {
+            // Message was likely deleted by the user, rather than automatically
+            return;
+        }
+
+        const author = message.author;
+        const replies = this.replies.get(author);
+
+        // Get the reply that was made to this message, if it exists
+        const reply_cache = replies?.find(reply => reply.reply_to == message.id);
+        if (reply_cache) {
+            try {
+                await reply_cache.reply.delete();
+            } catch (e) {
+                // If the message was already deleted, we don't need to do anything
+            }
+
+            // Remove the reply from the cache
+            this.replies.set(author, replies?.filter(reply => reply.reply_to !== message.id) ?? []);
         }
     }
 }
