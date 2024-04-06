d368c97aef67ca746d09464997944c0a39fa7302
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-05 21:18:42 -0500

    Add a module for blocking invite links, closes #74

---
+++ src/components/invite-links.ts
@@ -0,0 +1,44 @@
+import { strict as assert } from "assert";
+
+import * as Discord from "discord.js";
+
+import { BotComponent } from "../bot-component.js";
+import { departialize } from "../utils/discord.js";
+
+const INVITE_RE = /\.gg\/\S+/gi;
+
+export function should_block(content: string) {
+    return INVITE_RE.test(content);
+}
+
+/**
+ * Invite link blocking
+ */
+export default class InviteLinks extends BotComponent {
+    static override get is_freestanding() {
+        return true;
+    }
+
+    async handle_message(message: Discord.Message) {
+        if (should_block(message.content)) {
+            const quote = await this.wheatley.make_quote_embeds([message]);
+            await message.delete();
+            await message.channel.send(`<@${message.author.id}> Please do not send invite links`);
+            await this.wheatley.channels.staff_flag_log.send({
+                content: `:warning: Invite link deleted`,
+                ...quote,
+            });
+        }
+    }
+
+    override async on_message_create(message: Discord.Message) {
+        await this.handle_message(message);
+    }
+
+    override async on_message_update(
+        old_message: Discord.Message | Discord.PartialMessage,
+        new_message: Discord.Message | Discord.PartialMessage,
+    ) {
+        await this.handle_message(await departialize(new_message));
+    }
+}
