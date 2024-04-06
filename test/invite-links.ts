ab4bcb138cbd2557ed422ed376f15db1c3972db1
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-06 11:39:09 -0500

    Add invite link tests

---
+++ test/invite-links.ts
@@ -0,0 +1,106 @@
+import { describe, expect, it } from "vitest";
+
+import { should_block } from "../src/components/invite-links.js";
+
+describe("invite link tests", () => {
+    it("should block invite links", () => {
+        expect(should_block("foobar https://discord.gg/randomserver foobar")).to.equal(true);
+        expect(should_block("foobar discord.gg/randomserver foobar")).to.equal(true);
+        expect(should_block("foobar .gg/randomserver foobar")).to.equal(true);
+        expect(should_block("foobar .gg/T897FfR foobar")).to.equal(true);
+        expect(should_block("foobar .GG/random foobar")).to.equal(true);
+        expect(should_block("foobar .gg/12*(*^&^)asdggascn foobar")).to.equal(true);
+        expect(should_block(".gg/1")).to.equal(true);
+        expect(
+            should_block(`
+!wp
+# What Is Template Instantiation?
+
+Templates are not real entities until a piece of code uses them with arguments.
+When this happens, the compiler replaces the template parameters with the
+provided arguments, deriving the generic code into specific code.
+
+The generic code needs to be available in any translation unit that uses it. This
+is why templates are typically declared **and** defined entirely in headers.
+
+<!-- inline -->
+## Template function
+\`\`\`cpp
+template<typename T>
+void print(T arg) {
+    std::cout << arg << ' ';
+}
+\`\`\`
+
+<!-- inline -->
+## Usage
+?inline
+\`\`\`cpp
+.gg/randomserver
+print(42);
+print("Hello");
+\`\`\`
+
+## Substitution
+On the first call to \`print\`, the compiler substitutes \`T\` for \`int\` in the
+template code, _instantiating_ it into a new function. The second call to \`print\`
+causes \`T\` to be substituted for \`const char*\`, instantiating the template again.
+
+Here, \`T\` is said to be _deduced_: the compiler infers \`T\` from the type of the
+argument to \`print\`.
+
+**Output:**
+\`\`\`
+42 Hello
+\`\`\``),
+        ).to.equal(true);
+    });
+
+    it("should not block normal messages", () => {
+        expect(should_block("foobar")).to.equal(false);
+        expect(
+            should_block(`
+!wp
+# What Is Template Instantiation?
+
+Templates are not real entities until a piece of code uses them with arguments.
+When this happens, the compiler replaces the template parameters with the
+provided arguments, deriving the generic code into specific code.
+
+The generic code needs to be available in any translation unit that uses it. This
+is why templates are typically declared **and** defined entirely in headers.
+
+<!-- inline -->
+## Template function
+\`\`\`cpp
+template<typename T>
+void print(T arg) {
+    std::cout << arg << ' ';
+}
+\`\`\`
+
+<!-- inline -->
+## Usage
+?inline
+\`\`\`cpp
+print(42);
+print("Hello");
+\`\`\`
+
+## Substitution
+On the first call to \`print\`, the compiler substitutes \`T\` for \`int\` in the
+template code, _instantiating_ it into a new function. The second call to \`print\`
+causes \`T\` to be substituted for \`const char*\`, instantiating the template again.
+
+Here, \`T\` is said to be _deduced_: the compiler infers \`T\` from the type of the
+argument to \`print\`.
+
+**Output:**
+\`\`\`
+42 Hello
+\`\`\``),
+        ).to.equal(false);
+        expect(should_block(".gg/")).to.equal(false);
+        expect(should_block("foo .gg/ bar")).to.equal(false);
+    });
+});
