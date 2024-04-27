472f9ff220a7c0f861661d4c845b8fbb514f0279
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-27 11:56:35 -0500

    Add llm autoreply

---
+++ src/components/auto-reply.ts
@@ -0,0 +1,33 @@
+import * as Discord from "discord.js";
+
+import { strict as assert } from "assert";
+
+import { M } from "../utils/debugging-and-logging.js";
+import { colors, MINUTE } from "../common.js";
+import { BotComponent } from "../bot-component.js";
+import { Wheatley } from "../wheatley.js";
+import { TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
+import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
+
+const LLM_REGEX = /\b(?<!!)llm\b/gi;
+
+const RATELIMIT = 5 * MINUTE;
+
+/**
+ * Adds autoreplies
+ */
+export default class Autoreply extends BotComponent {
+    static override get is_freestanding() {
+        return true;
+    }
+
+    last_reply = 0; // ratelimit
+
+    override async on_message_create(message: Discord.Message) {
+        if (LLM_REGEX.test(message.content) && Date.now() - this.last_reply >= RATELIMIT) {
+            M.log("firing llm auto-reply");
+            this.last_reply = Date.now();
+            await message.reply("Did you mean: [***LLVM***](https://llvm.org/)");
+        }
+    }
+}
