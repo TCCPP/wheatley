42db836f4e1f6a71cd0e87b59d67a11cda10b64d
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-29 19:34:56 -0500

    Update ratelimit for llm autoreply

--- src/components/auto-reply.ts
+++ src/components/auto-reply.ts
@@ -3,11 +3,12 @@ import * as Discord from "discord.js";
 import { strict as assert } from "assert";
 
 import { M } from "../utils/debugging-and-logging.js";
-import { colors, MINUTE } from "../common.js";
+import { colors, DAY, HOUR, MINUTE } from "../common.js";
 import { BotComponent } from "../bot-component.js";
 import { Wheatley } from "../wheatley.js";
 import { TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
 import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
+import { SelfClearingSet } from "../utils/containers.js";
 
 const LLM_REGEX = /\b(?<!!)llms?\b/gi;
 
@@ -21,12 +22,13 @@ export default class Autoreply extends BotComponent {
         return true;
     }
 
-    last_reply = 0; // ratelimit
+    // set of channel ids
+    ratelimit = new SelfClearingSet<string>(DAY, HOUR);
 
     override async on_message_create(message: Discord.Message) {
-        if (LLM_REGEX.test(message.content) && Date.now() - this.last_reply >= RATELIMIT) {
+        if (LLM_REGEX.test(message.content) && !this.ratelimit.has(message.channelId)) {
+            this.ratelimit.insert(message.channelId);
             M.log("firing llm auto-reply");
-            this.last_reply = Date.now();
             await message.reply("Did you mean: [***LLVM***](https://llvm.org/)");
         }
     }
