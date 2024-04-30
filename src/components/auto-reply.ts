5dae5a3250134c8faff5308ea871fb2f8c80ee3a
Jeremy <51220084+jeremy-rifkin@users.noreply.github.com>
2024-04-29 19:36:08 -0500

    Cleanup unused crap

--- src/components/auto-reply.ts
+++ src/components/auto-reply.ts
@@ -3,16 +3,11 @@ import * as Discord from "discord.js";
 import { strict as assert } from "assert";
 
 import { M } from "../utils/debugging-and-logging.js";
-import { colors, DAY, HOUR, MINUTE } from "../common.js";
+import { DAY, HOUR } from "../common.js";
 import { BotComponent } from "../bot-component.js";
-import { Wheatley } from "../wheatley.js";
-import { TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
-import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
 import { SelfClearingSet } from "../utils/containers.js";
 
 const LLM_REGEX = /\b(?<!!)llms?\b/gi;
-
-const RATELIMIT = 5 * MINUTE;
 
 /**
  * Adds autoreplies
