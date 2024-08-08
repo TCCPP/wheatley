30fb745f05af9eaf04eca33b003050871d8ea068
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2024-08-07 19:22:52 -0500

    Only notify on formerly kicked/banned users if the case isn't expunged

--- src/components/notify-about-formerly-banned-users.ts
+++ src/components/notify-about-formerly-banned-users.ts
@@ -38,6 +38,7 @@ export default class NotifyAboutFormerlyBannedUsers extends BotComponent {
             {
                 user: member.id,
                 $or: [{ type: "ban" }, { type: "softban" }, { type: "kick" }],
+                expunged: null,
             },
             {
                 sort: {
