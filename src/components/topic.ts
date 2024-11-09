45c78e8a6e11461df77d2c57a2a62e1378a979db
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2024-11-08 19:26:20 -0600

    Add !topic command

---
+++ src/components/topic.ts
@@ -0,0 +1,40 @@
+import * as Discord from "discord.js";
+
+import { strict as assert } from "assert";
+
+import { M } from "../utils/debugging-and-logging.js";
+import { colors } from "../common.js";
+import { BotComponent } from "../bot-component.js";
+import { Wheatley, create_error_reply } from "../wheatley.js";
+import { EarlyReplyMode, TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
+import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
+import { build_description } from "../utils/strings.js";
+
+export default class Topic extends BotComponent {
+    static override get is_freestanding() {
+        return true;
+    }
+
+    constructor(wheatley: Wheatley) {
+        super(wheatley);
+
+        this.add_command(
+            new TextBasedCommandBuilder("topic", EarlyReplyMode.none)
+                .set_description("Posts the channel topic")
+                .set_handler(this.topic.bind(this)),
+        );
+    }
+
+    async topic(command: TextBasedCommand) {
+        const channel = await command.get_channel();
+        if (channel.isDMBased() || channel.isThread() || channel.isVoiceBased()) {
+            await command.reply("Must be used in a guild text-based non-thread channel");
+            return;
+        }
+        if (channel.topic) {
+            await command.reply(channel.topic);
+        } else {
+            await command.reply("No channel description set");
+        }
+    }
+}
