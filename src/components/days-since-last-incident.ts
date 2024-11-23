eb8221257b83663228bc37ad30329cf9720b793f
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2024-11-23 14:55:30 -0600

    Make DaysSinceLastIncident include culprit and punishment

--- src/components/days-since-last-incident.ts
+++ src/components/days-since-last-incident.ts
@@ -5,10 +5,12 @@ import { strict as assert } from "assert";
 import { BotComponent } from "../bot-component.js";
 import { Wheatley } from "../wheatley.js";
 import { unwrap } from "../utils/misc.js";
-import { time_to_human } from "../utils/strings.js";
+import { build_description, capitalize, time_to_human } from "../utils/strings.js";
 import { MINUTE } from "../common.js";
 import { moderation_entry } from "../infra/schemata/moderation.js";
 import { set_interval } from "../utils/node.js";
+
+type incident_info = { time: string; user: string; user_name: string; type: string };
 
 export default class DaysSinceLastIncident extends BotComponent {
     message: Discord.Message | null = null;
@@ -20,35 +22,38 @@ export default class DaysSinceLastIncident extends BotComponent {
         this.wheatley.event_hub.on("issue_moderation", this.handle_incident.bind(this));
     }
 
-    make_embed(time: string) {
-        const [count, unit] = time.split(" ");
+    make_embed(incident: incident_info) {
+        const [count, unit] = incident.time.split(" ");
         return new Discord.EmbedBuilder()
             .setColor(0xefd30a)
-            .setDescription(`# \`${count}\` \`${unit}\` since last incident`);
+            .setDescription(
+                build_description(
+                    `# \`${count}\` \`${unit}\` since last incident`,
+                    `**Culprit:** <@${incident.user}> ("${incident.user_name}")`,
+                    `**Punishment:** ${capitalize(incident.type)}`,
+                ),
+            );
     }
 
-    async time_since_last_incident() {
+    async last_incident_info(): Promise<incident_info> {
         const moderations = await this.wheatley.database.moderations
             .find({ type: { $ne: "note" } })
             .sort({ issued_at: -1 })
             .limit(1)
             .toArray();
         assert(moderations.length == 1);
-        const last_incident = moderations[0].issued_at;
-        const delta = Date.now() - last_incident;
-        if (delta < MINUTE) {
-            return "0 minutes";
-        } else {
-            return time_to_human(delta, 1);
-        }
+        const last_incident = moderations[0];
+        const delta = Date.now() - last_incident.issued_at;
+        const time = delta < MINUTE ? "0 minutes" : time_to_human(delta, 1);
+        return { time, user: last_incident.user, user_name: last_incident.user_name, type: last_incident.type };
     }
 
     async update_or_send_if_needed() {
-        const time = await this.time_since_last_incident();
-        if (time !== this.last_time) {
-            this.last_time = time;
+        const info = await this.last_incident_info();
+        if (info.time !== this.last_time) {
+            this.last_time = info.time;
             await unwrap(this.message).edit({
-                embeds: [this.make_embed(time)],
+                embeds: [this.make_embed(info)],
             });
         }
     }
@@ -62,9 +67,10 @@ export default class DaysSinceLastIncident extends BotComponent {
             this.message = unwrap(messages.first());
             await this.update_or_send_if_needed();
         } else {
-            this.last_time = await this.time_since_last_incident();
+            const info = await this.last_incident_info();
+            this.last_time = info.time;
             await this.wheatley.channels.days_since_last_incident.send({
-                embeds: [this.make_embed(this.last_time)],
+                embeds: [this.make_embed(info)],
             });
         }
         this.timer = set_interval(() => {
