31e38176db37357d2fc03e16d098c290050d8f0c
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2024-12-11 21:51:58 -0600

    Update insult logic

--- src/components/insult.ts
+++ src/components/insult.ts
@@ -9,22 +9,34 @@ export default class Insult extends BotComponent {
         return true;
     }
 
+    has_unkind_words(message: Discord.Message) {
+        return (
+            message.content.match(/shut up/gi) ||
+            message.content.match(/bad bot/gi) ||
+            message.content.match(/get a life/gi) ||
+            message.content.match(/fuck (off|you)/gi)
+        );
+    }
+
     override async on_message_create(message: Discord.Message) {
+        let is_unkind = false;
         if (message.mentions.has(this.wheatley.id)) {
             if (message.content.match(/\bk\s*y\s*s\b/gi)) {
                 await message.reply("Please don't say that");
                 return;
             }
-            if (
-                message.content.match(/shut up/gi) ||
-                message.content.match(/bad bot/gi) ||
-                message.content.match(/get a life/gi) ||
-                message.content.match(/fuck (off|you)/gi)
-            ) {
-                await message.reply({
-                    files: ["https://i.pinimg.com/736x/b4/26/17/b42617b777837f1cb6f189b434492a7e.jpg"],
-                });
+            if (this.has_unkind_words(message)) {
+                is_unkind = true;
             }
+        } else {
+            if (this.has_unkind_words(message) && message.content.match(/wheatley/gi)) {
+                is_unkind = true;
+            }
+        }
+        if (is_unkind) {
+            await message.reply({
+                files: ["https://i.pinimg.com/736x/b4/26/17/b42617b777837f1cb6f189b434492a7e.jpg"],
+            });
         }
     }
 }
