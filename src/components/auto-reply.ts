ade8f9d5d59c5d2c7b7ea0ce6b6a7a894aa3f52e
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-02-07 21:56:18 -0600

    Disable llm auto

--- src/components/auto-reply.ts
+++ src/components/auto-reply.ts
@@ -9,6 +9,8 @@ import { SelfClearingSet } from "../utils/containers.js";
 
 const LLM_REGEX = /\b(?<!!)llms?\b/gi;
 
+const LLM_AUTOREPLY_ENABLED = false;
+
 export default class Autoreply extends BotComponent {
     static override get is_freestanding() {
         return true;
@@ -18,7 +20,8 @@ export default class Autoreply extends BotComponent {
     ratelimit = new SelfClearingSet<string>(DAY, HOUR);
 
     override async on_message_create(message: Discord.Message) {
-        if (LLM_REGEX.test(message.content) && !this.ratelimit.has(message.channelId)) {
+        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+        if (LLM_AUTOREPLY_ENABLED && LLM_REGEX.test(message.content) && !this.ratelimit.has(message.channelId)) {
             this.ratelimit.insert(message.channelId);
             M.log("firing llm auto-reply");
             await message.reply("Did you mean: [***LLVM***](https://llvm.org/)");
