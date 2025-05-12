b6e82dd5db06c057c0db5a0cdd30c78c1212c461
Michael Kenzel <michael.kenzel@gmail.com>
2025-05-12 04:55:05 +0200

    Move utility tools

--- src/modules/tccpp/components/utility-tools.ts
+++ src/modules/tccpp/components/utility-tools.ts
@@ -1,13 +1,13 @@
 import * as Discord from "discord.js";
 import { strict as assert } from "assert";
-import { M } from "../utils/debugging-and-logging.js";
-import { BotComponent } from "../bot-component.js";
-import { CommandSetBuilder } from "../command-abstractions/command-set-builder.js";
-import { Wheatley } from "../wheatley.js";
-import { EarlyReplyMode, TextBasedCommandBuilder } from "../command-abstractions/text-based-command-builder.js";
-import { TextBasedCommand } from "../command-abstractions/text-based-command.js";
+import { M } from "../../../utils/debugging-and-logging.js";
+import { BotComponent } from "../../../bot-component.js";
+import { CommandSetBuilder } from "../../../command-abstractions/command-set-builder.js";
+import { Wheatley } from "../../../wheatley.js";
+import { EarlyReplyMode, TextBasedCommandBuilder } from "../../../command-abstractions/text-based-command-builder.js";
+import { TextBasedCommand } from "../../../command-abstractions/text-based-command.js";
 import { markdown_node, MarkdownParser } from "dismark";
-import { Synopsinator } from "../utils/synopsis.js";
+import { Synopsinator } from "../../../utils/synopsis.js";
 
 /**
  * Provides TCCPP-specific utilities for renaming channels etc.
