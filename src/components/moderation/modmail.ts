be621e25f10eb90743521daf8a97a90bc43fbdb8
Aryan Beezadhur <aryanbeezadhur@gmail.com>
2025-06-09 15:43:34 +0100

    Remove redundant "system

--- src/components/moderation/modmail.ts
+++ src/components/moderation/modmail.ts
@@ -187,7 +187,7 @@ export default class Modmail extends BotComponent {
         await interaction.reply({
             content:
                 "Hello and welcome to Together C&C++ :wave: Please read before pressing buttons and only " +
-                "use the modmail system system when there is an __issue requiring staff attention__.",
+                "use the modmail system when there is an __issue requiring staff attention__.",
             ephemeral: true,
         });
         await this.log_action(interaction.member, "Monkey pressed the button");
