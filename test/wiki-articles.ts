d3159eb82dae705a309225fea80f267e2a55cc02
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-08-16 22:42:41 -0500

    Fix tests

--- test/wiki-articles.ts
+++ test/wiki-articles.ts
@@ -7,7 +7,7 @@ import { globSync } from "glob";
 import { parse_article, wiki_articles_path } from "../src/components/wiki.js";
 
 describe("parse wiki articles", () => {
-    for (const file_path of globSync(`${wiki_articles_path}/articles/**/*.md`, { withFileTypes: true })) {
+    for (const file_path of globSync(`${wiki_articles_path}/**/*.md`, { withFileTypes: true })) {
         test(`${file_path.name} article should parse`, async () => {
             const content = await fs.promises.readFile(file_path.fullpath(), { encoding: "utf-8" });
             parse_article(null, content, str => str);
