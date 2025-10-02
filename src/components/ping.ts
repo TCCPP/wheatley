eb6440e905a5a573eefc82ed6fe0f64efa6b1d2d
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2025-10-01 20:34:16 -0500

    Remove !wstatus alias

--- src/components/ping.ts
+++ src/components/ping.ts
@@ -17,7 +17,7 @@ export default class Ping extends BotComponent {
 
     override async setup(commands: CommandSetBuilder) {
         commands.add(
-            new TextBasedCommandBuilder(["ping", "wstatus"], EarlyReplyMode.none)
+            new TextBasedCommandBuilder("ping", EarlyReplyMode.none)
                 .set_category("Misc")
                 .set_description("Ping")
                 .set_handler(this.ping.bind(this)),
