80cc0d45e0659f24b37915a3faffe44bfb351d89
ianpike <ianpike98@gmail.com>
2026-01-04 11:38:53 -0500

    Also check for booster status or skill role above beginner

--- src/modules/wheatley/components/moderation/voice.ts
+++ src/modules/wheatley/components/moderation/voice.ts
@@ -14,6 +14,7 @@ import { TextBasedCommand } from "../../../../command-abstractions/text-based-co
 import { SelfClearingSet } from "../../../../utils/containers.js";
 import { build_description } from "../../../../utils/strings.js";
 import { unwrap } from "../../../../utils/misc.js";
+import SkillRoles, { SkillLevel } from "../../../tccpp/components/skill-roles.js";
 
 type voice_first_join_notice_entry = {
     guild: string;
@@ -115,6 +116,17 @@ export default class VoiceModeration extends BotComponent {
                         .set_handler(this.wrap_command_handler(this.handle_quarantine.bind(this))),
                 ),
         );
+    }
+
+    private has_skill_role_above_beginner(member: Discord.GuildMember) {
+        const skill_roles_component = this.wheatley.components.get("SkillRoles");
+        if (skill_roles_component && skill_roles_component instanceof SkillRoles) {
+            return skill_roles_component.find_highest_skill_level(member) > SkillLevel.beginner;
+        }
+
+        // If the SkillRoles component isn't loaded, check by role name.
+        const higher_skill_role_names = new Set(["intermediate", "proficient", "advanced", "expert"]);
+        return member.roles.cache.some(role => higher_skill_role_names.has(role.name.toLowerCase()));
     }
 
     private wrap_command_handler<Args extends unknown[] = []>(
@@ -296,6 +308,8 @@ export default class VoiceModeration extends BotComponent {
                 res.upsertedCount > 0 &&
                 !member.roles.cache.has(this.wheatley.roles.voice.id) &&
                 !member.roles.cache.has(this.wheatley.roles.no_voice.id) &&
+                !member.roles.cache.has(this.wheatley.roles.server_booster.id) &&
+                !this.has_skill_role_above_beginner(member) &&
                 new_state.channel?.isVoiceBased()
             ) {
                 await new_state.channel.send({
@@ -304,7 +318,7 @@ export default class VoiceModeration extends BotComponent {
                         "new users are suppressed by default to protect our voice channels. " +
                         "You will be able to speak when joining a channel with a voice moderator present. " +
                         "Stick around and you will eventually be granted permanent voice access. " +
-                        "Please do not ping voice moderators to be unsupressed.",
+                        "Please do not ping voice moderators to be unsupressed or for the voice role.",
                     allowedMentions: { users: [member.id] },
                 });
             }
