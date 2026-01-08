1ce8635c6a86ac5c9965352a8e2a0f5c28d1d312
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-07 20:14:20 -0600

    Update excludes

--- scripts/scp.sh
+++ scripts/scp.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #scp -r package.json package-lock.json tsconfig.json src d0:Projects/Wheatley
-rsync -av --checksum package.json package-lock.json start.sh run-persist.sh tsconfig.json indexes src test wiki config.jsonc scripts x0:projects/wheatley --exclude={"indexes/cppref/*.txt","indexes/cppref/*cppreference*","*.js","src/wheatley-private/.git"}
+rsync -av --checksum package.json package-lock.json start.sh run-persist.sh tsconfig.json indexes src test wiki config.jsonc scripts x0:projects/wheatley --exclude={"indexes/cppref/*.txt","indexes/cppref/*cppreference*","*.js","src/wheatley-private/.git","wiki/node_modules"}
 #rsync -av -I package.json package-lock.json tsconfig.json cppref src test d0:Projects/Wheatley --exclude={"cppref/*.txt","cppref/*cppreference*"}
 
 # scp d0:Projects/wheatley/log.txt .
