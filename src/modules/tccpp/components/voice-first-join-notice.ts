ea328c1560efcf21d0a89b5617f1d44fd005fb73
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-11 20:31:17 -0600

    Clean up voice first join notice logic and exempt mods

--- src/modules/tccpp/components/voice-first-join-notice.ts
+++ src/modules/tccpp/components/voice-first-join-notice.ts
@@ -3,6 +3,7 @@ import * as Discord from "discord.js";
 
 import { BotComponent } from "../../../bot-component.js";
 import SkillRoles, { SkillLevel } from "./skill-roles.js";
+import { assert_type, unwrap } from "../../../utils/misc.js";
 
 type voice_first_join_notice_entry = {
     guild: string;
@@ -16,28 +17,25 @@ export default class VoiceFirstJoinNotice extends BotComponent {
         voice_first_join_notice: voice_first_join_notice_entry;
     }>();
 
+    skill_roles_component!: SkillRoles;
+
     override async setup() {
         await this.database.voice_first_join_notice.createIndex({ guild: 1, user: 1 }, { unique: true });
+        this.skill_roles_component = assert_type(unwrap(this.wheatley.components.get("SkillRoles")), SkillRoles);
     }
 
     override async on_voice_state_update(old_state: Discord.VoiceState, new_state: Discord.VoiceState) {
-        if (old_state.channelId != null || new_state.channelId == null) {
+        if (
+            new_state.guild.id !== this.wheatley.guild.id ||
+            old_state.channelId != null ||
+            new_state.channelId == null ||
+            new_state.channelId === this.wheatley.guild.afkChannelId ||
+            !new_state.member ||
+            new_state.member.user.bot
+        ) {
             return;
         }
-
-        if (new_state.guild.id !== this.wheatley.guild.id) {
-            return;
-        }
-
         const member = new_state.member;
-        if (!member || member.user.bot) {
-            return;
-        }
-
-        if (new_state.channelId === this.wheatley.guild.afkChannelId) {
-            return;
-        }
-
         const res = await this.database.voice_first_join_notice.updateOne(
             { guild: new_state.guild.id, user: member.id },
             {
@@ -50,34 +48,23 @@ export default class VoiceFirstJoinNotice extends BotComponent {
             },
             { upsert: true },
         );
-
         if (res.upsertedCount === 0) {
             return;
         }
-
-        if (member.roles.cache.has(this.wheatley.roles.voice.id)) {
+        if (
+            member.roles.cache.has(this.wheatley.roles.voice.id) ||
+            member.roles.cache.has(this.wheatley.roles.no_voice.id) ||
+            member.roles.cache.has(this.wheatley.roles.server_booster.id) ||
+            (await this.wheatley.check_permissions(member, Discord.PermissionFlagsBits.BanMembers)) ||
+            (await this.wheatley.check_permissions(member, Discord.PermissionFlagsBits.MuteMembers)) ||
+            this.skill_roles_component.find_highest_skill_level(member) > SkillLevel.beginner
+        ) {
             return;
         }
-
-        if (member.roles.cache.has(this.wheatley.roles.no_voice.id)) {
-            return;
-        }
-
-        if (member.roles.cache.has(this.wheatley.roles.server_booster.id)) {
-            return;
-        }
-
-        const skill_roles_component = this.wheatley.components.get("SkillRoles");
-        assert(skill_roles_component && skill_roles_component instanceof SkillRoles, "SkillRoles component missing");
-        if (skill_roles_component.find_highest_skill_level(member) > SkillLevel.beginner) {
-            return;
-        }
-
         const channel = new_state.channel;
         if (!channel) {
             return;
         }
-
         await channel.send({
             content:
                 `<@${member.id}> ` +
