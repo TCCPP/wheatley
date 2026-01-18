9fdc47a0ee3c3f4931f5572a28d5691bad5d1563
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-17 23:25:33 -0600

    Use source map info in logging utilities

--- src/utils/debugging-and-logging.ts
+++ src/utils/debugging-and-logging.ts
@@ -1,15 +1,36 @@
+import { findSourceMap } from "node:module";
+
 import moment from "moment";
 import chalk from "chalk";
 import * as stacktrace from "stack-trace";
 
+function map_to_source(file: string, line: number, column: number): { file: string; line: number; column: number } {
+    const source_map = findSourceMap(file);
+    if (source_map) {
+        const origin = source_map.findEntry(line, column);
+        if ("originalSource" in origin) {
+            return {
+                file: origin.originalSource,
+                line: origin.originalLine,
+                column: origin.originalColumn,
+            };
+        }
+    }
+    return { file, line, column };
+}
+
 function get_caller_location() {
     // https://stackoverflow.com/a/53339452/15675011
     const trace = stacktrace.get();
-    const file = trace[2].getFileName();
-    const line = trace[2].getLineNumber();
-    const col = trace[2].getColumnNumber();
+    const raw_file = trace[2].getFileName();
+    const raw_line = trace[2].getLineNumber();
+    const raw_col = trace[2].getColumnNumber();
     const fn: string | null = trace[2].getFunctionName();
-    return `${fn || ""} ${file}:${line}:${col}`.trim();
+    if (!raw_file) {
+        return fn || "<unknown>";
+    }
+    const { file, line, column } = map_to_source(raw_file, raw_line, raw_col);
+    return `${fn || ""} ${file}:${line}:${column}`.trim();
 }
 
 export class M {
