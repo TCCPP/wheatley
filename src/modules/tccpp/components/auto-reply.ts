983d74068193a7f8fac32467b62e1eec9e70bd23
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-23 22:56:15 -0600

    Add ms autoreply

--- src/modules/tccpp/components/auto-reply.ts
+++ src/modules/tccpp/components/auto-reply.ts
@@ -8,8 +8,10 @@ import { BotComponent } from "../../../bot-component.js";
 import { SelfClearingSet } from "../../../utils/containers.js";
 
 const LLM_REGEX = /\b(?<!!)llms?\b/gi;
+const MICROSLOP_REGEX = /\b(?<!!)microsoft?\b/gi;
 
 const LLM_AUTOREPLY_ENABLED = false;
+const MICROSLOP_AUTOREPLY_ENABLED = true;
 
 export default class Autoreply extends BotComponent {
     static override get is_freestanding() {
@@ -28,6 +30,16 @@ export default class Autoreply extends BotComponent {
             this.ratelimit.insert(message.channelId);
             M.log("firing llm auto-reply");
             await message.reply("Did you mean: [***LLVM***](https://llvm.org/)");
+        }
+        if (
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+            MICROSLOP_AUTOREPLY_ENABLED &&
+            MICROSLOP_REGEX.test(message.content) &&
+            !this.ratelimit.has(message.channelId)
+        ) {
+            this.ratelimit.insert(message.channelId);
+            M.log("firing microslop auto-reply");
+            await message.reply("Did you mean: ***Microslop***");
         }
         if (
             message.mentions.has(this.wheatley.user.id) &&
