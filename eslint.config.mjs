b372fa3e2f55c23493e2e5ad6a894a145306ce33
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-24 10:59:17 -0600

    Format eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -12,88 +12,115 @@ const __dirname = path.dirname(__filename);
 const compat = new FlatCompat({
     baseDirectory: __dirname,
     recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
+    allConfig: js.configs.all,
 });
 
-export default [{
-    ignores: ["**/scratch", "**/*.js", "**/indexes"],
-}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-        prettier,
+export default [
+    {
+        ignores: ["**/scratch", "**/*.js", "**/indexes"],
     },
-
-    languageOptions: {
-        globals: {
-            ...globals.browser,
+    ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"),
+    {
+        plugins: {
+            "@typescript-eslint": typescriptEslint,
+            prettier,
         },
 
-        parser: tsParser,
-        ecmaVersion: "latest",
-        sourceType: "module",
+        languageOptions: {
+            globals: {
+                ...globals.browser,
+            },
 
-        parserOptions: {
-            project: "./tsconfig.json",
+            parser: tsParser,
+            ecmaVersion: "latest",
+            sourceType: "module",
+
+            parserOptions: {
+                project: "./tsconfig.json",
+            },
+        },
+
+        rules: {
+            "max-len": ["error", 120],
+            semi: ["error", "always"],
+            "no-unused-vars": ["off"],
+            "no-constant-condition": ["off"],
+
+            "semi-spacing": [
+                "error",
+                {
+                    before: false,
+                    after: true,
+                },
+            ],
+
+            "no-empty": [
+                "error",
+                {
+                    allowEmptyCatch: true,
+                },
+            ],
+
+            "object-curly-spacing": ["error", "always"],
+            "comma-spacing": ["error"],
+            "computed-property-spacing": ["error"],
+
+            "brace-style": [
+                "error",
+                "1tbs",
+                {
+                    allowSingleLine: true,
+                },
+            ],
+
+            "eol-last": ["error"],
+            "semi-style": ["error", "last"],
+
+            "prefer-const": [
+                "error",
+                {
+                    destructuring: "all",
+                },
+            ],
+
+            curly: ["error"],
+            "prettier/prettier": "warn",
+            "@typescript-eslint/no-unused-vars": ["off"],
+            "@typescript-eslint/no-non-null-assertion": ["off"],
+            "@typescript-eslint/no-explicit-any": ["off"],
+            "@typescript-eslint/no-unnecessary-type-assertion": ["error"],
+
+            "@typescript-eslint/no-unnecessary-condition": [
+                "error",
+                {
+                    allowConstantLoopConditions: true,
+                },
+            ],
+
+            "@typescript-eslint/no-this-alias": [
+                "error",
+                {
+                    allowedNames: ["self"],
+                },
+            ],
+
+            "@typescript-eslint/no-floating-promises": ["error"],
+            "@typescript-eslint/no-misused-promises": ["error"],
+            "@typescript-eslint/await-thenable": ["error"],
+
+            "@typescript-eslint/naming-convention": [
+                "error",
+                {
+                    selector: "variable",
+                    format: ["snake_case", "UPPER_CASE", "snake_case"],
+                    leadingUnderscore: "allow",
+                },
+                {
+                    selector: "variable",
+                    format: null,
+                    modifiers: ["destructured"],
+                },
+            ],
         },
     },
-
-    rules: {
-        "max-len": ["error", 120],
-        semi: ["error", "always"],
-        "no-unused-vars": ["off"],
-        "no-constant-condition": ["off"],
-
-        "semi-spacing": ["error", {
-            before: false,
-            after: true,
-        }],
-
-        "no-empty": ["error", {
-            allowEmptyCatch: true,
-        }],
-
-        "object-curly-spacing": ["error", "always"],
-        "comma-spacing": ["error"],
-        "computed-property-spacing": ["error"],
-
-        "brace-style": ["error", "1tbs", {
-            allowSingleLine: true,
-        }],
-
-        "eol-last": ["error"],
-        "semi-style": ["error", "last"],
-
-        "prefer-const": ["error", {
-            destructuring: "all",
-        }],
-
-        curly: ["error"],
-        "prettier/prettier": "warn",
-        "@typescript-eslint/no-unused-vars": ["off"],
-        "@typescript-eslint/no-non-null-assertion": ["off"],
-        "@typescript-eslint/no-explicit-any": ["off"],
-        "@typescript-eslint/no-unnecessary-type-assertion": ["error"],
-
-        "@typescript-eslint/no-unnecessary-condition": ["error", {
-            allowConstantLoopConditions: true,
-        }],
-
-        "@typescript-eslint/no-this-alias": ["error", {
-            allowedNames: ["self"],
-        }],
-
-        "@typescript-eslint/no-floating-promises": ["error"],
-        "@typescript-eslint/no-misused-promises": ["error"],
-        "@typescript-eslint/await-thenable": ["error"],
-
-        "@typescript-eslint/naming-convention": ["error", {
-            selector: "variable",
-            format: ["snake_case", "UPPER_CASE", "snake_case"],
-            leadingUnderscore: "allow",
-        }, {
-            selector: "variable",
-            format: null,
-            modifiers: ["destructured"],
-        }],
-    },
-}];
+];
