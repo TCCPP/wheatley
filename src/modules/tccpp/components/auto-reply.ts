4e14cf25be82e3df0b86b31b9e0992560368a332
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-25 21:18:38 -0600

    Minor auto-reply tweaks

--- src/modules/tccpp/components/auto-reply.ts
+++ src/modules/tccpp/components/auto-reply.ts
@@ -9,7 +9,7 @@ const MICROSLOP_REGEX = /\b(?<!!)microsoft?\b/gi;
 
 const LLM_AUTOREPLY_ENABLED = false;
 const MICROSLOP_AUTOREPLY_ENABLED = true;
-const RATELIMIT_DURATION = 4 * HOUR;
+const RATELIMIT_DURATION = 6 * HOUR;
 
 export default class Autoreply extends BotComponent {
     static override get is_freestanding() {
@@ -30,7 +30,10 @@ export default class Autoreply extends BotComponent {
         if (LLM_AUTOREPLY_ENABLED && LLM_REGEX.test(message.content) && !this.is_ratelimited()) {
             this.last_reply_time = Date.now();
             M.log("firing llm auto-reply");
-            await message.reply("Did you mean: [***LLVM***](https://llvm.org/)");
+            await message.reply({
+                content: "Did you mean: [***LLVM***](https://llvm.org/)",
+                allowedMentions: { repliedUser: false },
+            });
         }
         if (
             // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
@@ -40,7 +43,10 @@ export default class Autoreply extends BotComponent {
         ) {
             this.last_reply_time = Date.now();
             M.log("firing microslop auto-reply");
-            await message.reply("Did you mean: [***Microslop***](https://microslop.com/)");
+            await message.reply({
+                content: "Did you mean: [***Microslop***](<https://microslop.com/>)",
+                allowedMentions: { repliedUser: false },
+            });
         }
         if (
             message.mentions.has(this.wheatley.user.id) &&
