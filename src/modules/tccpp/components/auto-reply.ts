68ffb3a53b6c25e7dc27c4ece9cb80e5200b155e
Jeremy Rifkin <51220084+jeremy-rifkin@users.noreply.github.com>
2026-01-26 20:12:10 -0600

    Update autoreply ratelimiting

--- src/modules/tccpp/components/auto-reply.ts
+++ src/modules/tccpp/components/auto-reply.ts
@@ -1,7 +1,7 @@
 import * as Discord from "discord.js";
 
 import { M } from "../../../utils/debugging-and-logging.js";
-import { HOUR } from "../../../common.js";
+import { DAY, HOUR } from "../../../common.js";
 import { BotComponent } from "../../../bot-component.js";
 
 const LLM_REGEX = /\b(?<!!)llms?\b/gi;
@@ -9,14 +9,15 @@ const MICROSLOP_REGEX = /\b(?<!!)microsoft?\b/gi;
 
 const LLM_AUTOREPLY_ENABLED = false;
 const MICROSLOP_AUTOREPLY_ENABLED = true;
-const RATELIMIT_DURATION = 6 * HOUR;
+const RATELIMIT_DURATION = 1 * DAY;
+const RATELIMIT_PROBABILITY = 0.2;
 
 export default class Autoreply extends BotComponent {
     static override get is_freestanding() {
         return true;
     }
 
-    last_reply_time = 0;
+    last_reply_time = Date.now() + RATELIMIT_DURATION; // Hedge against restarts
 
     is_ratelimited() {
         return Date.now() - this.last_reply_time < RATELIMIT_DURATION;
@@ -26,8 +27,14 @@ export default class Autoreply extends BotComponent {
         if (message.guildId !== this.wheatley.guild.id || message.author.bot) {
             return;
         }
-        // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
-        if (LLM_AUTOREPLY_ENABLED && LLM_REGEX.test(message.content) && !this.is_ratelimited()) {
+        if (
+            // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+            LLM_AUTOREPLY_ENABLED &&
+            LLM_REGEX.test(message.content) &&
+            !this.is_ratelimited() &&
+            Math.random() <= RATELIMIT_PROBABILITY &&
+            message.channel.id !== this.wheatley.channels.bot_spam
+        ) {
             this.last_reply_time = Date.now();
             M.log("firing llm auto-reply");
             await message.reply({
@@ -39,7 +46,9 @@ export default class Autoreply extends BotComponent {
             // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
             MICROSLOP_AUTOREPLY_ENABLED &&
             MICROSLOP_REGEX.test(message.content) &&
-            !this.is_ratelimited()
+            !this.is_ratelimited() &&
+            Math.random() <= RATELIMIT_PROBABILITY &&
+            message.channel.id !== this.wheatley.channels.bot_spam
         ) {
             this.last_reply_time = Date.now();
             M.log("firing microslop auto-reply");
