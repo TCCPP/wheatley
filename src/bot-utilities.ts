TCCPP/wheatley

commit c41bdb7d382f939e3a627b82c5a16745a5f7daf0

Improve development experience (#199)
This PR improves the development experience by checking for channel and
role names while not being in production mode and resolves them with
given channel name.

src/bot-utilities.ts
@@ -2,10 +2,11 @@ import { strict as assert } from "assert";
 
 import * as Discord from "discord.js";
 
-import { Wheatley } from "./wheatley.js";
+import { named_id, Wheatley } from "./wheatley.js";
 import { decode_snowflake, is_media_link_embed, make_url, get_thread_owner } from "./utils/discord.js";
 import { unwrap } from "./utils/misc.js";
 import { colors } from "./common.js";
+import { is_string } from "./utils/strings.js";
 
 type quote_options = {
     // description template
@@ -336,39 +337,68 @@ export class BotUtilities {
         );
     }
 
-    async get_channel(id: string) {
-        const channel = await this.wheatley.client.channels.fetch(id);
-        if (!channel) {
-            throw Error(`Channel ${id} not found`);
+    private async get_channel_internal(channel_info: named_id, case_insensitive: boolean = true) {
+        let channel: Discord.GuildBasedChannel | null = null;
+
+        try {
+            channel = await this.wheatley.guild.channels.fetch(channel_info.id);
+        } catch (e) {
+            // don't throw when DiscordAPIError[50001]: Missing Access
+            if (e instanceof Discord.DiscordAPIError && e.code == 50001) {
+                // unknown channel
+                channel = null;
+            } else {
+                throw e;
+            }
         }
-        assert(channel instanceof Discord.TextChannel, `Channel ${channel} (${id}) not of the expected type`);
+
+        if (this.wheatley.devmode_enabled && !channel && channel_info.name && is_string(channel_info.name)) {
+            channel = this.get_channel_by_name(channel_info.name, case_insensitive) ?? null;
+        }
+
         return channel;
     }
 
-    async get_forum_channel(id: string) {
-        const channel = await this.wheatley.client.channels.fetch(id);
-        if (!channel) {
-            throw Error(`Forum channel ${id} not found`);
-        }
-        assert(channel instanceof Discord.ForumChannel, `Channel ${channel} (${id}) not of the expected type`);
+    async get_channel(channel_info: named_id) {
+        const channel = await this.get_channel_internal(channel_info);
+        assert(
+            channel instanceof Discord.TextChannel,
+            `Channel ${channel?.name} (${channel_info.id}) not of the expected type`,
+        );
+
         return channel;
     }
 
-    async get_thread_channel(id: string) {
-        const channel = await this.wheatley.client.channels.fetch(id);
-        if (!channel) {
-            throw Error(`Thread channel ${id} not found`);
-        }
-        assert(channel instanceof Discord.ThreadChannel, `Channel ${channel} (${id}) not of the expected type`);
+    async get_forum_channel(channel_info: named_id) {
+        const channel = await this.get_channel_internal(channel_info);
+
+        assert(
+            channel instanceof Discord.ForumChannel,
+            `Channel ${channel?.name} (${channel_info.id}) not of the expected type`,
+        );
+
         return channel;
     }
 
-    async get_category(id: string) {
-        const category = await this.wheatley.client.channels.fetch(id);
-        if (!category) {
-            throw Error(`Category ${id} not found`);
-        }
-        assert(category instanceof Discord.CategoryChannel, `Category ${category} (${id}) not of the expected type`);
+    async get_thread_channel(channel_info: named_id) {
+        const channel = await this.get_channel_internal(channel_info);
+
+        assert(
+            channel instanceof Discord.ThreadChannel,
+            `Channel ${channel?.name} (${channel_info.id}) not of the expected type`,
+        );
+
+        return channel;
+    }
+
+    async get_category(channel_info: named_id) {
+        const category = await this.get_channel_internal(channel_info, false);
+
+        assert(
+            category instanceof Discord.CategoryChannel,
+            `Channel ${category?.name} (${channel_info.id}) not of the expected type`,
+        );
+
         return category;
     }
 
@@ -387,5 +417,21 @@ export class BotUtilities {
                 throw e;
             }
         }
+    }
+
+    // case-insensitive
+    get_channel_by_name(name: string, case_insensitive: boolean = true) {
+        return this.wheatley.guild.channels.cache.find(channel => {
+            if (case_insensitive) {
+                return channel.name.toLowerCase() === name.toLowerCase();
+            }
+
+            return channel.name === name;
+        });
+    }
+
+    // case-insensitive
+    get_role_by_name(name: string) {
+        return this.wheatley.guild.roles.cache.find(role => role.name.toLowerCase() === name.toLowerCase());
     }
 }

src/modules/tccpp/components/anti-self-star.ts
@@ -26,7 +26,11 @@ export default class AntiSelfStar extends BotComponent {
         if (reaction.emoji.name !== "⭐") {
             return;
         }
-        if (message.channelId == this.wheatley.channels.memes && user.id == message.author.id && has_media(message)) {
+        if (
+            message.channelId == this.wheatley.channels.memes.id &&
+            user.id == message.author.id &&
+            has_media(message)
+        ) {
             await this.handle_self_star(await departialize(message));
         }
     }

src/modules/tccpp/components/auto-reply.ts
@@ -33,7 +33,7 @@ export default class Autoreply extends BotComponent {
             LLM_REGEX.test(message.content) &&
             !this.is_ratelimited() &&
             Math.random() <= RATELIMIT_PROBABILITY &&
-            message.channel.id !== this.wheatley.channels.bot_spam
+            message.channel.id !== this.wheatley.channels.bot_spam.id
         ) {
             this.last_reply_time = Date.now();
             M.log("firing llm auto-reply");
@@ -48,7 +48,7 @@ export default class Autoreply extends BotComponent {
             MICROSLOP_REGEX.test(message.content) &&
             !this.is_ratelimited() &&
             Math.random() <= RATELIMIT_PROBABILITY &&
-            message.channel.id !== this.wheatley.channels.bot_spam
+            message.channel.id !== this.wheatley.channels.bot_spam.id
         ) {
             this.last_reply_time = Date.now();
             M.log("firing microslop auto-reply");

src/modules/tccpp/components/autoreact.ts
@@ -122,7 +122,7 @@ export default class Autoreact extends BotComponent {
                     this.wheatley.warn("Unable to find emoji nog4g");
                 }
             }
-            if (message.channel.id == this.wheatley.channels.introductions) {
+            if (message.channel.id == this.wheatley.channels.introductions.id) {
                 if (message.member == null) {
                     // TODO: Ping zelis?
                     M.warn("Why??", message);
@@ -132,15 +132,15 @@ export default class Autoreact extends BotComponent {
                     M.log("Waving to new user", message.author.tag, message.author.id, message.url);
                     await message.react("👋");
                 }
-            } else if (message.channel.id == this.wheatley.channels.memes && has_media(message)) {
+            } else if (message.channel.id == this.wheatley.channels.memes.id && has_media(message)) {
                 M.log("Adding star reaction", message.author.tag, message.author.id, message.url);
                 await message.react("⭐");
-            } else if (message.channel.id == this.wheatley.channels.server_suggestions) {
+            } else if (message.channel.id == this.wheatley.channels.server_suggestions.id) {
                 M.log("Adding server suggestion reactions", message.author.tag, message.author.id, message.url);
                 await message.react("👍");
                 await message.react("👎");
                 await message.react("🤷");
-            } else if (message.channel.id == this.wheatley.channels.food && has_media(message)) {
+            } else if (message.channel.id == this.wheatley.channels.food.id && has_media(message)) {
                 const reaction = message.guild!.emojis.cache.find(emoji => emoji.name === "chefskiss");
                 if (reaction !== undefined) {
                     await message.react(reaction);
@@ -152,7 +152,7 @@ export default class Autoreact extends BotComponent {
             // reaction blocked
             if (e instanceof Discord.DiscordAPIError && e.code === 90001) {
                 await message.member?.timeout(1 * MINUTE, "Thou shall not block the bot");
-                if (message.channel.id == this.wheatley.channels.server_suggestions) {
+                if (message.channel.id == this.wheatley.channels.server_suggestions.id) {
                     await message.delete();
                 }
             } else {
@@ -177,7 +177,7 @@ export default class Autoreact extends BotComponent {
         if (new_message.createdTimestamp && Date.now() - new_message.createdTimestamp > 5 * MINUTE) {
             return;
         }
-        if (new_message.channel.id == this.wheatley.channels.memes) {
+        if (new_message.channel.id == this.wheatley.channels.memes.id) {
             const bot_starred = new_message.reactions.cache.get("⭐")?.users.cache.has(this.wheatley.user.id);
             // If we haven't stared (or don't know if we've starred) and the new message has media, star
             if (!bot_starred && has_media(new_message)) {
@@ -204,10 +204,7 @@ export default class Autoreact extends BotComponent {
     }
 
     async catch_up() {
-        const TCCPP = await this.wheatley.client.guilds.fetch(this.wheatley.guild.id);
-        const introductions_channel = await TCCPP.channels.fetch(this.wheatley.channels.introductions);
-        assert(introductions_channel);
-        assert(introductions_channel.type == Discord.ChannelType.GuildText);
+        const introductions_channel = await this.utilities.get_channel(this.wheatley.channels.introductions);
         const messages = await introductions_channel.messages.fetch({ limit: 100, cache: false });
         for (const [_, message] of messages) {
             if (await this.is_new_member(message)) {

src/modules/tccpp/components/c-help-redirect.ts
@@ -190,8 +190,8 @@ export default class CHelpRedirect extends BotComponent {
         assert(command.channel instanceof Discord.GuildChannel);
 
         // Only allowed in #c-help-text
-        if (command.channel.id != this.wheatley.channels.c_help_text) {
-            await command.reply(`Can only be used in <#${this.wheatley.channels.c_help_text}>`, true);
+        if (command.channel.id != this.wheatley.channels.c_help_text.id) {
+            await command.reply(`Can only be used in <#${this.wheatley.channels.c_help_text.id}>`, true);
             return;
         }
 
@@ -200,12 +200,12 @@ export default class CHelpRedirect extends BotComponent {
         if (user) {
             await this.c_help_text.send(
                 `<@${user.id}> Your code looks like C++ code, but this is a C channel. ` +
-                    `Did you mean to post in <#${this.wheatley.channels.cpp_help_text}>?`,
+                    `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
             );
         } else {
             await this.c_help_text.send(
                 `This code looks like C++ code, but this is a C channel. ` +
-                    `Did you mean to post in <#${this.wheatley.channels.cpp_help_text}>?`,
+                    `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
             );
         }
     }
@@ -215,8 +215,8 @@ export default class CHelpRedirect extends BotComponent {
         assert(command.channel instanceof Discord.GuildChannel);
 
         // Only allowed in #cpp-help-text
-        if (command.channel.id != this.wheatley.channels.cpp_help_text) {
-            await command.reply(`Can only be used in <#${this.wheatley.channels.cpp_help_text}>`, true);
+        if (command.channel.id != this.wheatley.channels.cpp_help_text.id) {
+            await command.reply(`Can only be used in <#${this.wheatley.channels.cpp_help_text.id}>`, true);
             return;
         }
 
@@ -225,12 +225,12 @@ export default class CHelpRedirect extends BotComponent {
         if (user) {
             await this.cpp_help_text.send(
                 `<@${user.id}> Your code looks like C code, but this is a C++ channel. ` +
-                    `Did you mean to post in <#${this.wheatley.channels.c_help_text}>?`,
+                    `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
             );
         } else {
             await this.cpp_help_text.send(
                 `This code looks like C code, but this is a C++ channel. ` +
-                    `Did you mean to post in <#${this.wheatley.channels.c_help_text}>?`,
+                    `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
             );
         }
     }
@@ -256,20 +256,20 @@ export default class CHelpRedirect extends BotComponent {
         }
 
         // Only check messages in help-text channels
-        if (message.channel.id == this.wheatley.channels.c_help_text) {
+        if (message.channel.id == this.wheatley.channels.c_help_text.id) {
             if (this.check_message_for_cpp_code(message)) {
                 this.auto_triggered_users.insert(message.author.id);
                 await message.reply(
                     `<@${message.author.id}> Your code looks like C++ code, but this is a C channel. ` +
-                        `Did you mean to post in <#${this.wheatley.channels.cpp_help_text}>?`,
+                        `Did you mean to post in <#${this.wheatley.channels.cpp_help_text.id}>?`,
                 );
             }
-        } else if (message.channel.id == this.wheatley.channels.cpp_help_text) {
+        } else if (message.channel.id == this.wheatley.channels.cpp_help_text.id) {
             if (this.check_message_for_c_code(message)) {
                 this.auto_triggered_users.insert(message.author.id);
                 await message.reply(
                     `<@${message.author.id}> Your code looks like C code, but this is a C++ channel. ` +
-                        `Did you mean to post in <#${this.wheatley.channels.c_help_text}>?`,
+                        `Did you mean to post in <#${this.wheatley.channels.c_help_text.id}>?`,
                 );
             }
         }

src/modules/tccpp/components/forum-channels.ts
@@ -294,7 +294,9 @@ export default class ForumChannels extends BotComponent {
     is_non_forum_mirror_channel(thread: Discord.ThreadChannel) {
         return (
             thread.parentId != null &&
-            [this.wheatley.channels.code_review, this.wheatley.channels.showcase].includes(thread.parentId)
+            [this.wheatley.channels.code_review, this.wheatley.channels.showcase].some(
+                channel_info => channel_info.id === thread.parentId,
+            )
         );
     }
 
@@ -344,7 +346,7 @@ export default class ForumChannels extends BotComponent {
                 await this.mirror_forum_post(
                     message,
                     `New ${
-                        thread.parentId == this.wheatley.channels.code_review ? "code review" : "showcase"
+                        thread.parentId == this.wheatley.channels.code_review.id ? "code review" : "showcase"
                     } post: ${thread.name}`,
                     true,
                     this.general_discussion,

src/modules/tccpp/components/memes.ts
@@ -56,7 +56,7 @@ export default class Memes extends BotComponent {
             });
             await user.send({
                 content:
-                    `Your message in <#${this.wheatley.channels.memes}> was deleted because it didn't contain ` +
+                    `Your message in <#${this.wheatley.channels.memes.id}> was deleted because it didn't contain ` +
                     `any images, videos, or media embeds. This channel is for sharing memes only. For commentary ` +
                     `please open a thread.`,
                 embeds: quote.embeds,
@@ -88,7 +88,7 @@ export default class Memes extends BotComponent {
         if (message.guildId !== this.wheatley.guild.id) {
             return;
         }
-        if (message.channel.id !== this.wheatley.channels.memes) {
+        if (message.channel.id !== this.wheatley.channels.memes.id) {
             return;
         }
         if (await this.should_skip(message)) {
@@ -109,7 +109,7 @@ export default class Memes extends BotComponent {
         if (new_message.guildId !== this.wheatley.guild.id) {
             return;
         }
-        if (new_message.channel.id !== this.wheatley.channels.memes) {
+        if (new_message.channel.id !== this.wheatley.channels.memes.id) {
             return;
         }
         const message = await departialize(new_message);

commit 73fbd13f1bc99c81d0c1d01a5beaa21982ad8508

Fix small bug introduced by #199

src/bot-utilities.ts
@@ -338,10 +338,10 @@ export class BotUtilities {
     }
 
     private async get_channel_internal(channel_info: named_id, case_insensitive: boolean = true) {
-        let channel: Discord.GuildBasedChannel | null = null;
+        let channel: Discord.Channel | null = null;
 
         try {
-            channel = await this.wheatley.guild.channels.fetch(channel_info.id);
+            channel = await this.wheatley.client.channels.fetch(channel_info.id);
         } catch (e) {
             // don't throw when DiscordAPIError[50001]: Missing Access
             if (e instanceof Discord.DiscordAPIError && e.code == 50001) {
@@ -355,6 +355,8 @@ export class BotUtilities {
         if (this.wheatley.devmode_enabled && !channel && channel_info.name && is_string(channel_info.name)) {
             channel = this.get_channel_by_name(channel_info.name, case_insensitive) ?? null;
         }
+
+        assert(!channel || !channel.isDMBased());
 
         return channel;
     }